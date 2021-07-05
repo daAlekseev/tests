e0d291032e2b1eb1b549a93d3246a8b16e404175
Denis <flashka.true@gmail.com>
2021-07-06 00:56:40 +0300

    committed

---
+++ task1 and task3/js/alert.js
@@ -0,0 +1,11 @@
+$(document).keyup(function (e) {
+    if (e.keyCode == 37) {
+        alert('Клавиша влево!');
+    } else if (e.keyCode == 38) {
+        alert('Клавиша вверх!');
+    } else if (e.keyCode == 39) {
+        alert('Клавиша вправо!');
+    } else if (e.keyCode == 40) {
+        alert('Клавиша вниз!');
+    }
+});
